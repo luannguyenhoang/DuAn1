0b0d21e4b5c1fd648ad91308b88c75be4b3a9f6f
luannguyenhoang <127278147+luannguyenhoang@users.noreply.github.com>
2023-11-02 17:56:04 +0700

    abc

---
+++ src/main/java/com/mycompany/mavenproject5/Mavenproject5.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject5;
+
+/**
+ *
+ * @author hoang
+ */
+public class Mavenproject5 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
